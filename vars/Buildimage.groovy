9c63dcea994a7311dbddf0ff321df35adfe376df
Ahmedib0299@gmail.com <Ahmedib0299@gmail.com>
2022-03-04 16:57:55 +0000

    adding param to dockerbuild

--- vars/Buildimage.groovy
+++ vars/Buildimage.groovy
@@ -1,9 +1,9 @@
 #! /usr/bin/env groovy
-def call() {
+def call(string ImageName) {
   echo "Building the docker image..."
   withCredentials([usernamePassword(credentialsId: 'docker_hub', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
     sh "echo $PwD | docker login -u $USER --password-stdin"
-    sh 'docker build -t doc299/java-maven-app:1.1 .'
-    sh 'docker push doc299/java-maven-app:1.1'
+    sh "docker build -t $ImageName ."
+    sh "docker push $ImageName" 
   }
 }
--- vars/Buildjar.groovy
+++ vars/Buildjar.groovy
@@ -1,6 +1,6 @@
 #! /usr/bin/env groovy
 
 def call() {
-  echo "Building the app..."
+  echo "Building the app for brance $BRANCH_NAME"
   sh 'mvn package'
 }
